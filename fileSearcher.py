f0446b3d8636b36fb6387d7e37fbde2fa9e60cf9
Tunde Osborne <osbornetunde@gmail.com>
2018-10-02 22:19:13 +0100

    search implementation

--- fileSearcher.py
+++ fileSearcher.py
@@ -1,4 +1,7 @@
+import os
+import collections
 
+searchResult = collections.namedtuple('SearchResult', 'file, line, text')
 
 def main():
 	print_header()
@@ -10,18 +13,64 @@ def main():
 	text = get_search_text_from_user()
 	if not text:
 		print("We can't search for nothing!")
+		return
+
+	matches = search_folders(folder, text)
+	for m in matches:
+		print("----------- MATCH -----------")
+		print("file: " + m.file)
+		print("line: {}".format(m.line))
+		print("match: " + m.text.strip())
+		print()
+		# print(m)
 
 def print_header():
-	pass
+	print("--------------------------------")
+	print("       File Search App")
+	print("--------------------------------")
 
 def get_folder_from_user():
-	pass
+	folder = input('What folder do you want to search? ')
+	if not folder or not folder.strip():
+		return None
+
+	if not os.path.isdir(folder):
+		return None
+
+	return os.path.abspath(folder)
 
 def get_search_text_from_user():
-	pass
+	text = input("What are you searching for [single phrases only]? ")
+	return text.lower()
 
-def search_file():
-	pass
 
-if __name__ = __main__:
+
+def search_folders(folder, text):
+
+	all_matches = []
+	items = os.listdir(folder)
+
+	for item in items:
+		full_item = os.path.join(folder, item)
+		if os.path.isdir(full_item):
+			continue
+
+		matches = search_file(full_item, text)
+		all_matches.extend(matches)
+
+	return all_matches
+
+def search_file(filename, search_text):
+	matches = []
+
+	with open(filename, 'r', encoding='utf-8') as fin:
+
+		for line in fin:
+			if line.lower().find(search_text) >= 0:
+				m = searchResult(line=line_num, file=filename, text=line)
+				matches.append(m)
+
+		return matches
+
+if __name__ == '__main__':
 	main()
